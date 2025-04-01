084b96a98320189ba08abee99dc1e88458e24019
Stanley Stock <stanleystock01@gmail.com>
2025-04-01 21:54:49 +0200

    Init Ionic Project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <ion-app>
+    <p>CaloScan</p>
+    <!-- <HomePage /> -->
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import HomePage from "./views/HomePage.vue";
+</script>
